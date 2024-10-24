5fea7e0ddafefc73add43a768f7343f398f1de92
andrewreese16 <andrewreese1616@gmail.com>
2024-10-24 13:21:45 -0400

    finished lab

--- clues.sql
+++ clues.sql
@@ -7,9 +7,6 @@ WHERE region = 'Southern Europe'
 ORDER BY population ASC
 LIMIT 1;
 
-
-
-
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
 -- Write SQL query here
